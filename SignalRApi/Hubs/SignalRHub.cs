9c46d9d7209742df2b39af9f4c0b7102dff282ec
Feyza Cakmak <feyzacakmakk1@gmail.com>
2024-04-29 22:51:21 +0300

    İstatistik işlemlerine devam edildi

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -15,18 +15,61 @@ namespace SignalRApi.Hubs
 			_productService = productService;
 		}
 
-		public async Task SendCategoryCount()
-        {
-            var value = _categoryService.TCategoryCount();
-            await Clients.All.SendAsync("ReceiveCategoryCount", value);
-
-        }
-
-		public async Task SendProductCount()
+		public async Task SendStatistic()
 		{
+			var value = _categoryService.TCategoryCount();
+			await Clients.All.SendAsync("ReceiveCategoryCount", value);
+
 			var value2 = _productService.TProductCount();
 			await Clients.All.SendAsync("ReceiveProductCount", value2);
 
+			var value3 = _categoryService.TActiveCategoryCount();
+			await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+			var value4 = _categoryService.TPassiveCategoryCount();
+			await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+
+			var value5 = _productService.TProductCountByCategoryNameHamburger();
+			await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburger", value5);
+
+			var value6 = _productService.TProductCountByCategoryNameDrink();
+			await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", value6);
+
+			var value7 = _productService.TProductPriceAvg();
+			await Clients.All.SendAsync("ReceiveProductPriceAvg", value7.ToString("0.00" +"₺"));
+
+			var value8 = _productService.TProductNameByMaxPrice();
+			await Clients.All.SendAsync("ReceiveProductNameByMaxPrice", value8);
+
+			var value9 = _productService.TProductNameByMinPrice();
+			await Clients.All.SendAsync("ReceiveProductNameByMinPrice", value9);
+
+			var value10 = _productService.TProductNameByMinPrice();
+			await Clients.All.SendAsync("ReceiveProductNameByMinPrice", value10);
+
 		}
+
+		//public async Task SendCategoryCount()
+  //      {
+  //          var value = _categoryService.TCategoryCount();
+  //          await Clients.All.SendAsync("ReceiveCategoryCount", value);
+
+  //      }
+
+		//public async Task SendProductCount()
+		//{
+		//	var value2 = _productService.TProductCount();
+		//	await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+		//}
+
+
+		//public async Task ActivePassiveCategoryCount()
+		//{
+		//	var value3=_categoryService.TActiveCategoryCount();
+		//	var value4=_categoryService.TPassiveCategoryCount();
+		//	await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+		//	await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+		//}
 	}
 }
