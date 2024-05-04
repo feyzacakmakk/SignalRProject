04ab7409121c398d24a2f7569712afa82d03a901
Feyza Cakmak <feyzacakmakk1@gmail.com>
2024-05-04 21:13:28 +0300

    Sepet işlemlerine devam edildi

--- SignalRApi/Controllers/BasketController.cs
+++ SignalRApi/Controllers/BasketController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
+using SignalR.DtoLayer.BasketDto;
+using SignalR.EntityLayer.Entities;
 using SignalRApi.Models;
 
 namespace SignalRApi.Controllers
@@ -40,6 +42,22 @@ namespace SignalRApi.Controllers
                 TotalPrice = z.TotalPrice
             }).ToList();
             return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBasket(CreateBasketDto createBasketDto)
+        {
+            using var context = new SignalRContext();
+             _basketService.TAdd(new Basket()
+            {
+                ProductID = createBasketDto.ProductID,
+                Count = 1,
+                MenuTableID=3,
+                Price=context.Products.Where(x=>x.ProductID==createBasketDto.ProductID).Select(x=>x.Price).FirstOrDefault(),
+                TotalPrice=0
+                
+            });
+            return Ok();
         }
     }
 }
--- SignalRWebUI/Controllers/BasketsController.cs
+++ SignalRWebUI/Controllers/BasketsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.BasketDtos;
+using System.Text;
 
 namespace SignalRWebUI.Controllers
 {
@@ -22,6 +23,21 @@ namespace SignalRWebUI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBasket(CreateBasketDto createBasketDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            //istemci oluşturuyorum
+            var jsonData = JsonConvert.SerializeObject(createBasketDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7210/api/Basket/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
